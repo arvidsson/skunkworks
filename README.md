aaefdb8d1377b0df7f080ec0811283d22d982a88
arvidsson <arvidsson@gmail.com>
2023-06-07 08:46:42 +0200

    added sokol

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # skunkworks
 
-adventures in C
+adventures in C using single-header libs
+
+## deps
+
+* [sokol](https://github.com/floooh/sokol)
+
+## instructions
+
+### macos
+
+`clang src/main.c src/lib.m -o bin/main -Weverything -isysteminclude -framework Cocoa -framework QuartzCore -framework OpenGL -Wno-poison-system-directories`
--- src/main.c
+++ src/main.c
@@ -1,7 +1,49 @@
-#include <stdio.h>
+#include "sokol_gfx.h"
+#include "sokol_app.h"
+#include "sokol_log.h"
+#include "sokol_glue.h"
 
-int main(void)
+#define UNUSED(x) (void)(x)
+
+static sg_pass_action pass_action;
+
+static void init(void)
 {
-    printf("Hello, World!\n");
-    return 0;
+    sg_setup(&(sg_desc){
+        .context = sapp_sgcontext(),
+        .logger.func = slog_func,
+    });
+    pass_action = (sg_pass_action) {
+        .colors[0] = { .load_action=SG_LOADACTION_CLEAR, .clear_value={1.0f, 0.0f, 0.0f, 1.0f} }
+    };
+}
+
+static void frame(void)
+{
+    float g = pass_action.colors[0].clear_value.g + 0.01f;
+    pass_action.colors[0].clear_value.g = (g > 1.0f) ? 0.0f : g;
+    sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
+    sg_end_pass();
+    sg_commit();
+}
+
+static void cleanup(void)
+{
+    sg_shutdown();
+}
+
+sapp_desc sokol_main(int argc, char* argv[])
+{
+    UNUSED(argc);
+    UNUSED(argv);
+
+    return (sapp_desc){
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .width = 400,
+        .height = 300,
+        .window_title = "Clear Sample",
+        .logger.func = slog_func,
+    };
 }
